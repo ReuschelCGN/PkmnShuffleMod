2193988ed17955dd8d5d244fbaf3bd7cbd9925f5
root <root@v2202306201826231612.quicksrv.de>
2025-05-19 09:46:13 +0200

    fix: index

--- UICONS_128/index.js
+++ UICONS_128/index.js
@@ -1,17 +1,6 @@
 const fs = require('fs')
 
-/*
-Script should be placed in the root of the UICONS directory
-
-Easiest to either run it with 'node create_index.js' or add it to a package.json file like below then run 'npm start'
-  "scripts": {
-    "start": "node -e 'require(\"./create_index\").createIndex()'"
-  },
-
-Example Implementation: https://github.com/WatWowMap/wwm-uicons
-*/
-
-module.exports.createIndex = async function createIndex() {
+module.exports.update = async function update() {
   const sorter = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' })
   const checkFolders = async (folder) => {
     folder = folder.replace('//', '/')
@@ -27,8 +16,13 @@ module.exports.createIndex = async function createIndex() {
     }))
     if (!hasSubFolders) {
       newJson = files.filter(file => file.includes('.png')).sort(sorter.compare)
+    } else {
+      const tempJson = newJson
+      const sortedKeys = Object.keys(newJson).sort(sorter.compare)
+      newJson = {}
+      sortedKeys.forEach(key => (newJson[key] = tempJson[key]))
     }
-    fs.writeFileSync(`./${folder === './' ? '' : `${folder}/`}index.json`, JSON.stringify(newJson))
+    fs.writeFile(`./${folder === './' ? '' : `${folder}/`}index.json`, JSON.stringify(newJson, null, 2), 'utf8', () => {})
     return newJson
   }
   await checkFolders('./')
